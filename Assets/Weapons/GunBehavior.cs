4e0559c13706a89131378c0eb5fed779b123c19f
Constantin Piber <cp.piber@gmail.com>
2022-10-24 20:05:17 +0200

    Gun: Fix angle, don't offset bullet
    Clamp the gun angle to prevent shooting into self
    Don't offset bullet from gun, could allow bullet to spawn outside

--- Assets/Weapons/GunBehavior.cs
+++ Assets/Weapons/GunBehavior.cs
@@ -6,6 +6,7 @@ public class GunBehavior : MonoBehaviour
     [SerializeField] private float bulletSpeed = .5f;
     [SerializeField] private int bulletDamage = 10;
 
+    static float halfAngle = Mathf.PI / 2 - 0.1f;
     void Start() {
         
     }
@@ -14,14 +15,15 @@ public class GunBehavior : MonoBehaviour
         var mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var angle = Mathf.Atan2(mouseWorld.y - transform.position.y, mouseWorld.x - transform.position.x);
         if (transform.parent.localScale.x < 0) angle += Mathf.PI;
+        if (angle >= Mathf.PI) angle -= 2 * Mathf.PI; // make sure we are always in range [-pi,pi]
+        angle = Mathf.Clamp(angle, -halfAngle, halfAngle);
         transform.rotation = Quaternion.Euler(0, 0, angle * 180 / Mathf.PI);
     }
 
     public void Shoot() {
         var rot = transform.rotation;
         if (transform.parent.localScale.x < 0) rot *= Quaternion.Euler(0, 0, 180); // add 180deg to offset scale=-x
-        var forward = rot * Vector3.right; // "forward" in direction of rotation
-        var bullet = Instantiate(bulletPrefab, transform.position + forward, rot);
+        var bullet = Instantiate(bulletPrefab, transform.position, rot);
         var b = bullet.GetComponentInChildren<BulletBehavior>();
         b.speed = bulletSpeed;
         b.damage = bulletDamage;
