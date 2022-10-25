30d2c63d1c2827e3eabfb39efefa32ea000a731a
Constantin Piber <cp.piber@gmail.com>
2022-10-25 18:42:44 +0200

    Enemy spawning

--- Assets/Level/CameraBehavior.cs
+++ Assets/Level/CameraBehavior.cs
@@ -7,11 +7,10 @@ public class CameraBehavior : MonoBehaviour
     [SerializeField] private GameObject wallRight;
 
     private new Camera camera;
-    private GameObject player;
+    public GameObject player;
     private float z;
-    private float minY;
-    private float minX;
-    private float maxX;
+    public Rect cameraRect;
+    public Vector2 cameraSize;
 
     void Start() {
         camera = this.GetComponent<Camera>();
@@ -24,11 +23,10 @@ public class CameraBehavior : MonoBehaviour
 
         var zz = ground.transform.position.z - z;
         var bl = camera.ViewportToWorldPoint(new Vector3(0, 0, zz));
-        var tr = camera.ViewportToWorldPoint(new Vector3(1, 1, zz));
         var c = camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, zz));
-        minY = ground.transform.position.y + c.y - bl.y;
-        minX = wallLeft.transform.position.x + c.x - bl.x;
-        maxX = wallRight.transform.position.x - (tr.x - c.x);
+        cameraSize = new Vector2(c.x - bl.x, c.y - bl.y);
+        cameraRect = Rect.MinMaxRect(wallLeft.transform.position.x + cameraSize.x, ground.transform.position.y + cameraSize.y,
+                                     wallRight.transform.position.x - cameraSize.x, float.MaxValue);
     }
 
     void FixedUpdate() {
@@ -38,9 +36,9 @@ public class CameraBehavior : MonoBehaviour
 
     private Vector3 calculateCamPosition() {
         Vector3 focus = player.transform.position + Vector3.forward * z;
-        focus.x = Mathf.Clamp(focus.x, minX, maxX);
-        focus.y = Mathf.Max(focus.y, minY);
-        // Debug.Log($"{focus} [{minX},{maxX}] {minY}");
+        focus.x = Mathf.Clamp(focus.x, cameraRect.xMin, cameraRect.xMax);
+        focus.y = Mathf.Max(focus.y, cameraRect.yMin);
+        // Debug.Log($"{focus} {cameraRect}");
         return focus;
     }
 }
--- Assets/Level/LevelBehavior.cs
+++ Assets/Level/LevelBehavior.cs
@@ -2,10 +2,39 @@ using UnityEngine;
 
 public class LevelBehavior : MonoBehaviour
 {
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float enemyCooldown = 6.0f;
+    [SerializeField] private float enemyCooldownScale = 0.8f;
+    [SerializeField] private float enemyCooldownMin = 0.5f;
+    [SerializeField] private uint numEnemies = 5;
+
+    private float lastSpawn;
+    private CameraBehavior camB;
+    private SceneBehavior sceneB;
+
     void Start() {
+        camB = this.GetComponent<CameraBehavior>();
+        sceneB = Camera.main.GetComponent<SceneBehavior>();
+        lastSpawn = enemyCooldown - 0.1f;
     }
 
-    void Update() {
+    void FixedUpdate() {
+        lastSpawn += Time.deltaTime;
+        if (lastSpawn < enemyCooldown) return;
+        lastSpawn -= enemyCooldown;
+        enemyCooldown *= enemyCooldownScale;
+        if (enemyCooldown < enemyCooldownMin) enemyCooldown = enemyCooldownMin;
         
+        for (int i = 0; i < numEnemies; ++i) SpawnEnemy();
+    }
+
+    void SpawnEnemy() {
+        var spawnLeft = camB.player.transform.position.x >= camB.cameraRect.xMax - 20;
+        // spawn space is either entire left side (just outside screen) or right side
+        var spawnRect = new Vector2(spawnLeft ? camB.cameraRect.xMin - camB.cameraSize.x : Camera.main.transform.position.x + camB.cameraSize.x,
+                                    spawnLeft ? Camera.main.transform.position.x - camB.cameraSize.x : camB.cameraRect.xMax + camB.cameraSize.x);
+        var spawnPoint = new Vector3(Random.Range(spawnRect.x, spawnRect.y), 0, 0);
+        var enemy = Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+        enemy.GetComponent<HealthBehavior>().onDeath.AddListener(sceneB.EnemyKilled);
     }
 }
