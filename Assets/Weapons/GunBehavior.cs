d7cab7e11b9c31c1045872277f797693089ff0e0
Constantin Piber <cp.piber@gmail.com>
2022-10-25 19:05:15 +0200

    Gun cooldown

--- Assets/Weapons/GunBehavior.cs
+++ Assets/Weapons/GunBehavior.cs
@@ -5,11 +5,10 @@ public class GunBehavior : MonoBehaviour
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletSpeed = .5f;
     [SerializeField] private int bulletDamage = 10;
+    [SerializeField] private float cooldown = 0.1f;
+    private float activeCooldown = 0;
 
     static float halfAngle = Mathf.PI / 2 - 0.1f;
-    void Start() {
-        
-    }
 
     void FixedUpdate() {
         var mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -18,14 +17,18 @@ public class GunBehavior : MonoBehaviour
         if (angle >= Mathf.PI) angle -= 2 * Mathf.PI; // make sure we are always in range [-pi,pi]
         angle = Mathf.Clamp(angle, -halfAngle, halfAngle);
         transform.rotation = Quaternion.Euler(0, 0, angle * 180 / Mathf.PI);
+
+        activeCooldown = Mathf.Max(0, activeCooldown - Time.deltaTime);
     }
 
     public void Shoot() {
+        if (activeCooldown > 0.0f) return;
         var rot = transform.rotation;
         if (transform.parent.localScale.x < 0) rot *= Quaternion.Euler(0, 0, 180); // add 180deg to offset scale=-x
         var bullet = Instantiate(bulletPrefab, transform.position, rot);
         var b = bullet.GetComponent<BulletBehavior>();
         b.speed = bulletSpeed;
         b.damage = bulletDamage;
+        activeCooldown = cooldown;
     }
 }
